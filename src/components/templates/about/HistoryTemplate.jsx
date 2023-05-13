13d46b7e93d8741fdf2121fcd224409d5423e3ab
eugenia1984 <mariaeugeniacosta1@gmail.com>
2023-05-13 19:10:48 -0300

    # feat/#7-terminos-y-condiciones - Add the terms and conditions list

--- src/components/templates/about/HistoryTemplate.jsx
+++ src/components/templates/about/HistoryTemplate.jsx
@@ -1,5 +1,28 @@
+import { Box, Typography } from '@mui/material'
+import { TEXT_ABOUT } from './text'
+import './AgentsTemplate'
+
 const HistoryTemplate = () => {
-  return (<>History</>)
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        maxWidth: 890,
+        margin: '1rem auto',
+        padding: '1.5rem 0.75rem 3rem'
+      }}
+      id="history-bg"
+    >
+      {TEXT_ABOUT.map((text) => (
+        <Typography
+          key={text.id}
+          sx={{ padding: '0rem 2rem 1rem 2rem', lineHeight: '1.8' }}
+        >
+          {text.text}
+        </Typography>
+      ))}
+    </Box>
+  )
 }
 
 export default HistoryTemplate
---
+++ src/components/templates/about/text.js
@@ -0,0 +1,10 @@
+export const TEXT_ABOUT = [
+  {
+    id: 1,
+    text: 'Appartamentos nació hace más de 10 años con el objetivo de dar respuesta a las necesidades de los distintos actores del mercado inmobiliario.'
+  },
+  {
+    id: 2,
+    text: 'Nuestra dinámica de trabajo, sumada a la amplia capacitación de los profesionales que integran nuestro staff, hace que en un mismo lugar, nuestros clientes estén protegidos por una empresa que con solvencia interviene en todas las etapas de una transacción inmobiliaria.'
+  }
+]
