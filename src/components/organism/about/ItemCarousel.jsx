d152d260f1639eaf506bb14f75682fd900fc146e
eugenia1984 <mariaeugeniacosta1@gmail.com>
2023-05-15 19:09:05 -0300

    Continue de carousel, the arrows next and preview are pending

--- src/components/organism/about/ItemCarousel.jsx
+++ src/components/organism/about/ItemCarousel.jsx
@@ -1,13 +1,23 @@
-import { Paper } from '@mui/material'
+import { Box, Paper, Typography } from '@mui/material'
+import Headline from '../../atom/headline/Headline'
 
 const ItemCarousel = ({ item }) => {
   const { fullName, telephone, srcImg } = item
+
   return (
-    <Paper>
-      <h2>{fullName}</h2>
-      <img src={srcImg} alt={fullName} />
-      <p>{telephone}</p>
-    </Paper>
+    <Box sx={{ width: '80%', margin: '0 auto', maxWidth: '500px' }}>
+      <Paper elevation={3} sx={{ paddingTop: '24px' }}>
+        <Headline variant="h3" title={fullName} />
+        <img
+          src={srcImg}
+          alt={fullName}
+          style={{ width: '200px', margin: '0 150px', borderRadius: '50%' }}
+        />
+        <Typography align="center" paddingBottom={2}>
+          {telephone}
+        </Typography>
+      </Paper>
+    </Box>
   )
 }
 
